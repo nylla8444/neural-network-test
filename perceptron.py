60b99a6cc732ae5598668ad48e3723880ca3b67e
Allyn Ralf Ledesma <110903853+nylla8444@users.noreply.github.com>
2025-08-19 09:53:51 +0800

    feat: Perceptron source code

---
+++ perceptron.py
@@ -0,0 +1,34 @@
+
+"""
+
+To run:
+python .\perceptron.py
+
+
+To test:
+Change values of INPUTS and WEIGHTS
+
+"""
+
+# Step Function Threshold
+THRESHOLD = 1.5
+
+INPUTS = [1, 0, 1, 0, 1]
+
+# Every input should have a corresponding weight.
+WEIGHTS = [0.7, 0.6, 0.5, 0.3, 0.4]
+
+
+# Perceptron Calculation
+sum = 0
+
+for i in range(len(INPUTS)):
+    sum += INPUTS[i] * WEIGHTS[i]
+
+# Step Function activation. 1 is True, 0 is False.
+if sum > THRESHOLD:
+    print("Output: 1")
+else:
+    print("Output: 0")
+
+
